58dc3458f744c3c300458f281f9b087950a65643
Piotr Rudnicki <deamondev@gmail.com>
2022-09-11 14:05:56 +0200

    reducer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Fragment, useState } from "react";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 import { ReactDOM } from "react-dom";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
@@ -18,13 +19,13 @@ function App() {
   };
 
   return (
-    <Fragment>
+    <CartProvider>
       {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
